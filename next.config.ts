1ac2f03d25a60a0a16eaa043dc7a6e2a589de10c
GeniusNear <budnistudenta512@gmail.com>
2026-01-21 03:59:51 +0300

    update 1st work version

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    // Разрешаем SVG
+    dangerouslyAllowSVG: true,
+    // Настраиваем безопасность для SVG
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
